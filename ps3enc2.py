099ab860b36360de102779157c0a3e6735934551
Cynyr <andrew.frink@gmail.com>
2009-06-21 23:32:37 -0500

    working implementation now

--- ps3enc2.py
+++ ps3enc2.py
@@ -4,18 +4,91 @@ from time import time
 from subprocess import Popen,PIPE,STDOUT
 import re
 
-def Main(files):
-    audbitrate = "128kb"
-    for file in files:
-        print file
-        global nfn = gen_output_filename(file)
-        global sinfo = GetSourceInfo(file)
-        global tframes =  ToFrames(get_source_length(sinfo))
-        global audio_bitrate = "128kb"
-        global video_options = "-deinterlace -vcodec libx264 -vpre " +\
-                            "hq -crf 22 -threads 0 -level 41"
-        global [audio_options,audio_options2] = GetAudioOptions(info)
-        print [nfn, tframes, audio_options, audio_options2]
+global audio_bitrate
+global crf
+audio_bitrate = "256kb"
+crf = "20"
+
+def Main(files, sizeopts = None):
+    filesn = len(files)
+    for filenum in range(filesn):
+        file = files[filenum]
+        sys.stderr.write("Starting: " + file + "\n")
+        nfn = gen_output_filename(file)
+        sinfo = GetSourceInfo(file)
+        tframes =  ToFrames(get_source_length(sinfo))
+        ffmpeg_cmd = build_command_list(sinfo,file, nfn)
+        #sys.stderr.write(" ".join(ffmpeg_cmd))
+        proc = Popen(ffmpeg_cmd, stdout=PIPE,stderr=STDOUT,\
+                     shell=False,universal_newlines=True)
+        t1 = time()
+        while proc.poll() == None:
+            oline = proc.stdout.readline()
+            #debug_print_output_line(oline)
+            frame = re.search(r"(?<=frame=)\s*[0-9]+", oline)
+            fps = re.search(r"(?<=fps=)\s*[0-9]+", oline)
+            if frame != None and fps != None:
+                frame = int(frame.group(0).strip())
+                fps = int(fps.group(0).strip())
+                #sys.stderr.write(fps)
+                #sys.stderr.write(frame)
+                if fps == 0:
+                    fps = 1
+                eta = int((tframes - frame)/ fps)
+                percent = frame / tframes * 100
+                #print [eta, percent]
+                output(eta, percent, file, filenum +1, filesn)
+        t2=time()
+        sys.stdout.write(" "*79 + "\r")
+        sys.stdout.write(file " compleated in: " + sec_to_hms(str(t2-t1)))
+        #print proc.poll()
+
+def output(eta, percent, file, filenum=1, total_files=1):
+    """output(eta, percent, file, filenum, total_files)
+    "eta" in seconds, 
+    "file" as a string, The name of the file
+    "filenum" as an int, The current position in the list of files
+    "total_files" as an int, The total number of files in the run
+
+    To provide a different output type override this function.
+    """
+    sys.stdout.write(" " * 79 + "\r")
+    sys.stdout.write("file " + str(filenum) + "/" + str(total_files))
+    sys.stdout.write(" " + file)
+    sys.stdout.write(" " + sec_to_hms(eta) + ", %3.2f%%\r" % (percent))
+
+def sec_to_hms(seconds):
+    """returns a string that is in HH:MM:SS from the seconds passed"""
+    seconds = int(seconds)
+    hours = seconds/3600
+    minutes = (seconds%3600)/60
+    secs = (seconds%3600)%60
+    return "%0*d:%0*d:%0*d" % (2, hours, 2, minutes, 2, secs)
+
+
+def debug_print_output_line(line):
+    if line != "":
+        print line.strip("\n")
+
+def build_command_list(sinfo,src,dest):
+    cmd = []
+    cmd.extend(["ffmpeg", "-i", src])
+    maps = GetAudioMap(sinfo)
+    #print maps
+    cmd.extend(["-map", "0:0", "-map",maps[0]])
+    cmd.extend(["-deinterlace", "-vcodec", "libx264", "-vpre"])
+    cmd.extend(["hq", "-crf", crf, "-threads", "0", "-level", "41"])
+    cmd.extend(["-acodec", "libfaac", "-ac", "6", "-ab", audio_bitrate])
+    #comment this line after testing
+    cmd.extend(["-vframes", "3000", "-f", "rawvideo", "-y", "/dev/null"])
+    #uncomment after testing
+    #cmd.append(dest)
+    if maps[1] != "stereo" and maps[2] != "aac":
+        cmd.extend(["-map", maps[0]])
+        cmd.extend(["-acodec", "libfaac", "-ac", "2", "-ab", audio_bitrate])
+        cmd.extend(["-newaudio",])
+
+    return cmd
 
 def gen_output_filename(inname):
     """Generate an output file name, checking for collisions"""
@@ -29,7 +102,7 @@ def gen_output_filename(inname):
 
 def ToSeconds(s):
     """ToSeconds(string), "01:23:78.8" and converts it to seconds"""
-    time=length.split(':')
+    time=s.split(':')
     seconds=int(time[0])*3600
     seconds+= int(time[1])*60 
     seconds+= float(time[2])
@@ -55,24 +128,40 @@ def GetSourceInfo(fn):
     return p.communicate()[0]
 
 def GetAudioMap(info, track="0x80"):
-    """GetAudioMap(info), Returns [file:stream, type] from the info)"""
+    """GetAudioMap(info), Returns [file:stream, type, codec] from the info)"""
 
     regex = '^\s*Stream.*\[' + track + '\].*$'
     ptr = re.compile(regex, re.MULTILINE)
     s = ptr.search(info)
+    #print "Finding track 0x80 returned: %s" % (s,)
     s2 = None
+    t=None
+    c = None
     if s != None:
         s = s.group(0)
         m = re.search('(?<=#)([0-9]\.[0-9])', s)
         t = re.search('(?<!#)([0-9]\.[0-9])|(stereo)', s)
+        c = re.search('(?<=Audio: )aac(?=,)', s)
         if m != None:
             s2 = m.group(0).replace(".",":")
-            print s2
         if t != None:
             t = t.group(0)
-    return [s2,t]
+        if c != None:
+            c = c.group(0)
+    else:
+        ptr = re.compile(r"^\s*Stream .*0.1.*:.*$", re.MULTILINE)
+        s = ptr.search(info)
+        if s != None:
+            s2="0:1"
+            t = re.search('(?<!#)([0-9]\.[0-9])|(stereo)', s.group(0))
+            c = re.search('(?<=Audio: )aac(?=,)', s.group(0))
+            if t != None:
+                t = t.group(0)
+            if c != None:
+                c = c.group(0)
+    return [s2,t,c]
 
-def GetAudioMapStrings(info, track="0x80"):
+def GetMapStrings(info, track="0x80"):
     [map,type] = GetAudioMap(info)
     r = ["-map 0:0 -map %s" % (map,),]
     if type != "stereo":
@@ -99,12 +188,13 @@ def get_source_length(info):
     return s
 
 if __name__ == "__main__":
-    for file in sys.argv[1:]:
-        print file
-        global nfn = gen_output_filename(sys.argv[1])
-        global sinfo = GetSourceInfo(file)
-        global tframes =  ToFrames(get_source_length(sinfo))
-        global audbitrate = "128kb"
+    Main(sys.argv[1:])
+    #for file in sys.argv[1:]:
+        #print file
+        #global nfn = gen_output_filename(sys.argv[1])
+        #global sinfo = GetSourceInfo(file)
+        #global tframes =  ToFrames(get_source_length(sinfo))
+        #global audbitrate = "128kb"
         #print length
         #seconds = ToSeconds(length)
         #print seconds
